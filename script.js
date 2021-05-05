4be4fe79e17b6d6ff09aed5a7a4b272e344421ef
JoseIgnacioTapia <josignaciot@gmail.com>
2021-05-05 17:48:36 -0300

    New Project Added

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 /* ***** Menu ***** */
-((d) => {
-  const $btnMenu = d.querySelector(".menu-btn"),
-    $menu = d.querySelector(".menu");
+(d => {
+  const $btnMenu = d.querySelector('.menu-btn'),
+    $menu = d.querySelector('.menu');
 
-  $btnMenu.addEventListener("click", (e) => {
-    $btnMenu.firstElementChild.classList.toggle("none");
-    $btnMenu.lastElementChild.classList.toggle("none");
-    $menu.classList.toggle("is-active");
+  $btnMenu.addEventListener('click', e => {
+    $btnMenu.firstElementChild.classList.toggle('none');
+    $btnMenu.lastElementChild.classList.toggle('none');
+    $menu.classList.toggle('is-active');
   });
 
-  d.addEventListener("click", (e) => {
-    if (!e.target.matches(".menu a")) return false;
+  d.addEventListener('click', e => {
+    if (!e.target.matches('.menu a')) return false;
 
-    $btnMenu.firstElementChild.classList.remove("none");
-    $btnMenu.lastElementChild.classList.add("none");
-    $menu.classList.remove("is-active");
+    $btnMenu.firstElementChild.classList.remove('none');
+    $btnMenu.lastElementChild.classList.add('none');
+    $menu.classList.remove('is-active');
   });
 })(document);
 
 /* ***** ContactForm ***** */
-((d) => {
-  const $form = d.querySelector(".contact-form"),
-    $loader = d.querySelector(".contact-form-loader"),
-    $response = d.querySelector(".contact-form-response");
+(d => {
+  const $form = d.querySelector('.contact-form'),
+    $loader = d.querySelector('.contact-form-loader'),
+    $response = d.querySelector('.contact-form-response');
 
-  $form.addEventListener("submit", (e) => {
+  $form.addEventListener('submit', e => {
     e.preventDefault();
-    $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/josignaciot@gmail.com", {
-      method: "POST",
+    $loader.classList.remove('none');
+    fetch('https://formsubmit.co/ajax/josignaciot@gmail.com', {
+      method: 'POST',
       body: new FormData(e.target),
     })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-      .then((json) => {
+      .then(res => (res.ok ? res.json() : Promise.reject(res)))
+      .then(json => {
         console.log(json);
-        location.hash = "#thanks";
+        location.hash = '#thanks';
         $form.reset();
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         let message =
-          err.statusText || "An error occurred while sending, try again.";
+          err.statusText || 'An error occurred while sending, try again.';
         $response.querySelector(
-          "h3"
+          'h3'
         ).innerHTML = `Error ${err.status}: ${message}`;
       })
       .finally(() => {
-        $loader.classList.add("none");
+        $loader.classList.add('none');
         setTimeout(() => {
-          location.hash = "#close";
+          location.hash = '#close';
         }, 3000);
       });
   });
