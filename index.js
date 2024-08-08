89ad9356e79610bce35d2b0db16d3d19da5385b9
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-08 16:50:42 -0300

    Change background color scene

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 
 const container = document.getElementById('animation-container');
 const loader = document.getElementById('loader');
@@ -17,14 +18,39 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
+// Change background color scene
+scene.background = new THREE.Color(0x87ceeb);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshBasicMaterial({
-  color: 0xccff,
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  flatShading: true,
 });
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
-renderer.render(scene, camera);
+const wireMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
+mesh.add(wireMesh);
+
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+scene.add(hemiLight);
+
+function animate(t = 0) {
+  requestAnimationFrame(animate);
+  mesh.rotation.y = t * 0.0001;
+  renderer.render(scene, camera);
+  controls.update();
+}
+animate();
 
 // Show the animation container and hide the loader when Three.js is ready
 window.addEventListener('load', () => {
